3fe43aed6002d4ff33510f99777a07b5e8d33d45
Rajarajan <rajarajanclg@gmail.com>
2026-06-21 13:02:03 +0530

    first commit

---
+++ app/routes/home.tsx
@@ -0,0 +1,19 @@
+import type { Route } from "./+types/home";
+import Navbar from "../../components/Navbar";
+
+console.log("Navbar =", Navbar);
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "New React Router App" },
+    { name: "description", content: "Welcome to React Router!" },
+  ];
+}
+
+export default function Home() {
+  return(
+    <div className = "home">
+      <Navbar />
+     <h1 className="text-3xl font-bold text-red-600">Welcome to React Router!</h1>
+    </div>
+  );
+}
